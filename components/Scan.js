c836390db87ac8946df46afde5eaa0f8d8fcc0e6
May Phan <songmayphan@gmail.com>
2020-03-14 19:53:27 -0400

    Added questions

--- components/Scan.js
+++ components/Scan.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Alert, Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function Scan() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -15,7 +16,22 @@ function Scan() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`UPC code for this item is ${data}`);
+   // alert(`UPC code for this item is ${data}`);
+   console.log(`UPC code for this item is ${data}`)
+   // APi calls for $data, look up data item
+    Alert.alert(
+      'Item Scanned',
+      'Add to cart?',
+      [
+        {
+          text: 'No',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'Yes', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
   };
 
   if (hasPermission === null) {
@@ -27,19 +43,40 @@ function Scan() {
 
   return (
     <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-      }}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
+    style={{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+    }}>
+    <BarCodeScanner
+    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+    style={StyleSheet.absoluteFillObject}
+  />
+      {scanned &&
+      <Button
+        title={'Tap to Scan'} 
+        onPress={() => setScanned(false)} 
+      />}    
 
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
+  </View>
   );
 }
+//styles
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    maxHeight: 160,
+    maxWidth: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    backgroundColor: '#bdc667'
+  },
+  text: {
+    color: 'white',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+});
 
 export default Scan;
