1a33bcaa7e42fbe4ddd5f37cf93d3558d34f621e
Sandro Giannini Garcia <42614895+SGIANNINIGARCIA@users.noreply.github.com>
2020-04-16 13:16:50 -0400

    changed compare.js to call api and display list of chosen items
    changed compare.js to call api and display list of chosen items

--- components/Compare.js
+++ components/Compare.js
@@ -13,34 +13,63 @@ export default function Compare() {
  //redux with hooks 
  //use this variable to loop through the list in redux
  const items = useSelector(state => state.list)
+ console.log(items);
  const dispatch = useDispatch();
  //use this function to add to list
  const add_to_list = (item) => dispatch(addToList(item));
  //IMPORTTANT!!! CALL THIS FUNCTION TO DELETE FROM LIST
  const delete_item = (id) => dispatch(deleteFromList(id));
 
-    
- //might want to get rid of this context right here
-    const {List} = useContext(ListContext);
+ // comparing
+
+
+//  not final, just testing
+ useEffect(() =>  {
+  console.log("comparing");
+  fetch("http://127.0.0.1:3000/compare/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: items
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      
+    });
+  })
+
+
+
   
     return (
       <View style={styles.screen}>
-       <FlatList
-            data={List}
-            removeClippedSubviews={true}
-            keyExtractor={({ _id }, index) => _id}
-            renderItem={({ item }) => (
-              <View style={styles.itemList} >
-                <Text>Item: {item.NAME}</Text>
-                <Text>Manufacturer: {item.MANUFACTURER}</Text>
-                <View style={styles.addButtonContainer}>
-                  <TouchableOpacity onPress={()=> {addItem(item)}} style={styles.button} >
-                    <Text>Add Item</Text>
-                  </TouchableOpacity>
-                </View>
+         <FlatList
+          data={items}
+          removeClippedSubviews={true}
+          keyExtractor={({ _id }, index) => _id}
+          renderItem={({ item }) => (
+            <View style={styles.itemList}>
+              <Text>Item: {item.name}</Text>
+              <Text>manufacturer: {item.manufacturer}</Text>
+              <View style={styles.addButtonContainer}>
+                <TouchableOpacity
+                  onPress={() => 
+                    //caaling redux right here
+                    delete_item(item.id)
+                  }
+                  style={styles.button}
+                >
+                  <Text>Delete</Text>
+                </TouchableOpacity>
               </View>
-            )}
-          />
+              </View>
+          )}
+        />
       </View>
     );
   };
